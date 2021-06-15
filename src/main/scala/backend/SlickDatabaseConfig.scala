4795e99086f13b80f01c8ed2b5c778dbee14f6c6
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-06-15 23:22:51 +0900

    Create SlickDatabaseConfig

---
+++ src/main/scala/backend/SlickDatabaseConfig.scala
@@ -0,0 +1,11 @@
+package backend
+
+import slick.jdbc.JdbcBackend.Database
+
+trait SlickDatabaseConfig {
+
+  val db: Database = Database.forConfig("slick.dbs.default.db")
+  val profile      = slick.jdbc.MySQLProfile
+}
+
+object SlickDatabaseConfig extends SlickDatabaseConfig
